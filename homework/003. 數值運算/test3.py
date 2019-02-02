0037f26f0b69d917b25e36268f99c800c71a719a
guyleaf <ychhua1@gmail.com>
2019-02-02 21:24:21 +0800

    change express method

--- homework/003. 數值運算/test3.py
+++ homework/003. 數值運算/test3.py
@@ -1,3 +1,20 @@
+"""
+003. 數值運算
+分別輸入 num1 num2 求出兩數的 Sum,Difference,Product,Quotient。
+結果須輸出到小數點第二位
+print("%.2f" %x1);
+
+輸入:
+25
+2
+
+輸出:
+Difference:23.00
+Sum:27.00
+Quotient:12.50
+Product:50.00
+"""
+
 import math
 def add(num1, num2): #加法
     sum = num1 + num2
@@ -8,7 +25,6 @@ def subtract(num1, num2): #減法
         difference = num1 - num2
     else:
         difference = num2 - num1
-
     return difference
 	
 def multiply(num1, num2): #乘法   
--- homework/005. BMI計算/test5.py
+++ homework/005. BMI計算/test5.py
@@ -1,3 +1,18 @@
+"""
+005. BMI計算
+輸入身高(公尺) 體重(公斤)(皆使用小數)
+BMI = 體重(公斤) / (身高*身高)(公尺)
+結果須輸出到小數點第一位
+print("%.1f" %x1);
+
+輸入:
+1.72
+60
+
+輸出:
+20.3
+"""
+
 def bmi(weight, height): #BMI計算
     BMI = weight / (height*height)
     return BMI
