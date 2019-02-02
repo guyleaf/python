c626ae5c4926b304a317a08cb8e55c83c1a2f57c
guyleaf <ychhua1@gmail.com>
2019-02-02 21:12:50 +0800

    test

--- homework/001. 計算總成績、平均/test1.py
+++ homework/001. 計算總成績、平均/test1.py
@@ -1,3 +1,23 @@
+"""
+001. 計算總成績、平均
+某一學生修國文、計算機概論、計算機程式設計三科，使用者輸入名字（一個char）、學號（int）、三科成績(int)。
+(1) 計算學生總成績、平均。
+(2) 印出名字、學號、總成績、平均。
+
+Input
+K
+905067
+100
+100
+100
+
+Output
+Name:K
+ID:905067
+Average:100
+Total:300
+"""
+
 def computescore(csi, pd, chinese): #計算成績 總平均, 總分
     average = (csi + pd + chinese) //3
     total = csi + pd + chinese
@@ -18,5 +38,4 @@ def output(name, id, average, total): #輸出
     print('Average:%d' %average)
     print('Total:%d' %total)
 
-	
 main()
